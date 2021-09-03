b8502ee53f6d3068b90c0bd9f962dac267f1390e
ArthurOrlov495 <90067862+ArthurOrlov495@users.noreply.github.com>
2021-09-03 23:28:49 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.querySelector('.burger').addEventListener('click', function(){
+    document.querySelector('#menu').classList.toggle('is-active')
+})
+
+document.querySelector('.burger__ws-menu').addEventListener('click', function(){
+    document.querySelector('#menu__ws-menu').classList.toggle('is-active')
+})
